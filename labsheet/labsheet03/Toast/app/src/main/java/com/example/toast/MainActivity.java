1607ddcb908a91aa248b43c3eafd9d2675682991
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-07 20:34:02 +0530

    toast completed

--- labsheet/labsheet03/Toast/app/src/main/java/com/example/toast/MainActivity.java
+++ labsheet/labsheet03/Toast/app/src/main/java/com/example/toast/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
 
-    EditText fname,lanme;
+    EditText fname,lname;
     Button show;
 
     @Override
@@ -18,8 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fname
-                
+        fname =(EditText)findViewById(R.id.txtfname);
+        lname=(EditText) findViewById(R.id.txtlname);
+        show=(Button) findViewById(R.id.btnshow);
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String efname=fname.getText().toString();
+                String elname=lname.getText().toString();
+                String fullname=efname + " "+ elname;
+
+                Toast.makeText(MainActivity.this,"Full name: " +fullname,Toast.LENGTH_LONG).show();
+
+
+
+
+
+            }
+        });
+
+
+
 
 
     }
