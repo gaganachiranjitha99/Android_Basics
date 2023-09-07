be8ca928f3a24ee34172728b6d28d3a8bab4a553
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-07 20:27:10 +0530

    ex4

---
+++ labsheet/labsheet03/Toast/app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    EditText fname,lanme;
+    Button show;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fname
+                
+
+
+    }
+}
