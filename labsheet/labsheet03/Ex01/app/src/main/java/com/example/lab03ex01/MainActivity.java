778ecf8156e980ed427097eb76fe06874e715d3c
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-07 13:29:35 +0530

    labsheet 03 exq1

---
+++ labsheet/labsheet03/Ex01/app/src/main/java/com/example/lab03ex01/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.lab03ex01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText name;
+    EditText address;
+    Button subbtn;
+    Bundle bundle;
+    Intent check;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        name=(EditText) findViewById(R.id.txtname);
+        address=(EditText) findViewById(R.id.txtaddress);
+        subbtn=(Button) findViewById(R.id.submitbtn);
+
+        check=new Intent(this, Activity2.class);
+        bundle=new Bundle();
+
+    }
+}
