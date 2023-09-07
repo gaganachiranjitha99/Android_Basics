054066c57a2688aeefb59d8150b3c41920acde67
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-07 15:58:42 +0530

    u[pdated

--- labsheet/labsheet03/BundleObjectex1/app/src/main/java/com/example/bundleobject_ex1/Activity2.java
+++ labsheet/labsheet03/BundleObjectex1/app/src/main/java/com/example/bundleobject_ex1/Activity2.java
@@ -3,12 +3,26 @@ package com.example.bundleobject_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
+
+
+    TextView name,indexno,depname,faculty;
+    Button submitbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+    name=(TextView) findViewById(R.id.txtname2);
+    
+
+
+
+
+
     }
 }
