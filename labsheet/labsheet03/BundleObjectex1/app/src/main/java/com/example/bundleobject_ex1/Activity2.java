9d621ffd4c26d57e7fad8bfe5f6b19f4853091db
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-07 19:43:10 +0530

    upadeted ex3

--- labsheet/labsheet03/BundleObjectex1/app/src/main/java/com/example/bundleobject_ex1/Activity2.java
+++ labsheet/labsheet03/BundleObjectex1/app/src/main/java/com/example/bundleobject_ex1/Activity2.java
@@ -2,9 +2,13 @@ package com.example.bundleobject_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -18,6 +22,29 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
 
     name=(TextView) findViewById(R.id.txtname2);
+    indexno=(TextView) findViewById(R.id.txtindex2);
+    depname=(TextView) findViewById(R.id.txtdepartment2);
+    faculty=(TextView) findViewById(R.id.txtfaculty2);
+    submitbtn=(Button) findViewById(R.id.btnsubmit);
+
+    submitbtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+
+            String ename= name.getText().toString();
+            String eindex=indexno.getText().toString();
+            String edepname=depname.getText().toString();
+            String efaculty=faculty.getText().toString();
+
+            Intent intent=new Intent(Activity2.this, Activity3.class);
+            intent.putExtra("name",ename);
+            intent.putExtra("name",ename);
+            intent.putExtra("name",ename);
+            intent.putExtra("name",ename);
+        }
+    });
+
     
 
 
