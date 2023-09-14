ef0c4a08b83ac327eb3a47803b51c35c86e262d5
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-14 14:36:47 +0530

    updated

--- labsheet/labsheet04/ex2/app/src/main/java/com/example/ex2/MainActivity.java
+++ labsheet/labsheet04/ex2/app/src/main/java/com/example/ex2/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView dispaly;
+    Button btn1;
+
+    static final int My_REQUEST_CODE=2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dispaly=(TextView) findViewById(R.id.displaya1);
+        btn1=(Button) findViewById(R.id.btna1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent  i =new Intent(MainActivity.this, two.class);
+                        startActivityForResult(i,My_REQUEST_CODE);
+
+            }
+        });
+
     }
 }
--- labsheet/labsheet04/ex2/app/src/main/java/com/example/ex2/two.java
+++ labsheet/labsheet04/ex2/app/src/main/java/com/example/ex2/two.java
@@ -3,12 +3,33 @@ package com.example.ex2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class two extends AppCompatActivity {
+
+
+    EditText edit;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+
+        edit=(EditText) findViewById(R.id.txta2);
+        btn=(Button) findViewById(R.id.btna2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+
     }
 }
