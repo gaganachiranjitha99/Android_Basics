7d88ab015023db3685a71643a7992fcdd1639844
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-14 15:41:36 +0530

    updated

--- labsheet/labsheet04/hwex1/app/src/main/java/com/example/hwex1/MainActivity.java
+++ labsheet/labsheet04/hwex1/app/src/main/java/com/example/hwex1/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.hwex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button gobtn;
+    private EditText urltxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        gobtn=(Button) findViewById(R.id.btngo);
+        urltxt=(EditText) findViewById(R.id.txturl);
+
+        gobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String enteredUrl=urltxt.getText().toString().trim();
+
+                
+
+
+
+            }
+        });
+
+
+
     }
 }
