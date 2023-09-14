97c44f70f8db2d2b17b4ab0e9a046e7813b4cfe2
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-14 15:44:23 +0530

    up[dated

--- labsheet/labsheet04/hwex1/app/src/main/java/com/example/hwex1/MainActivity.java
+++ labsheet/labsheet04/hwex1/app/src/main/java/com/example/hwex1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hwex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
                 String enteredUrl=urltxt.getText().toString().trim();
 
-                
+                Uri webpage =Uri.parse(enteredUrl);
+                Intent intent=new Intent(Intent.ACTION_VIEW,webpage);
 
 
 
