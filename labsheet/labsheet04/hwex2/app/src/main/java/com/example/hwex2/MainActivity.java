f51f10dd210e7389b5543e447464f80aa56bd413
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-18 22:58:51 +0530

    updated

--- labsheet/labsheet04/hwex2/app/src/main/java/com/example/hwex2/MainActivity.java
+++ labsheet/labsheet04/hwex2/app/src/main/java/com/example/hwex2/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.hwex2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView displayTotal;
+    Button gobtn;
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
