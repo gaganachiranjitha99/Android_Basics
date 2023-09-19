3ffd2fe69e3fa39353f3e7797d3b581aea4ebeaa
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-19 21:33:44 +0530

    Update secondActivity.java

--- labsheet/labsheet04/hwex2/app/src/main/java/com/example/hwex2/secondActivity.java
+++ labsheet/labsheet04/hwex2/app/src/main/java/com/example/hwex2/secondActivity.java
@@ -2,6 +2,7 @@ package com.example.hwex2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +35,7 @@ public class secondActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 double total=calculateItem();
+                Intent intent= new Intent();
 
             }
         });
@@ -46,6 +48,7 @@ public class secondActivity extends AppCompatActivity {
         total += parseDouble(et2.getText().toString());
         total += parseDouble(et3.getText().toString());
         total += parseDouble(et4.getText().toString());
+        total += parseDouble(et5.getText().toString());
 
         return total;
     }
