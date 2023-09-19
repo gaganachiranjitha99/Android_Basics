cc45988382faefe86da60ff03a088531488da80a
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-19 20:53:23 +0530

    Update MainActivity.java

--- labsheet/labsheet04/hwex2/app/src/main/java/com/example/hwex2/MainActivity.java
+++ labsheet/labsheet04/hwex2/app/src/main/java/com/example/hwex2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.hwex2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,7 +23,17 @@ public class MainActivity extends AppCompatActivity {
         displayTotal=(TextView) findViewById(R.id.totalTextView);
         gobtn=(Button) findViewById(R.id.btncart);
 
-        
+        gobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(MainActivity.this,secondActivity.class);
+
+
+
+
+            }
+        });
 
 
 
