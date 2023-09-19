7cd929251f2bdef4beaaa20c1f3089e16c00e140
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-19 21:11:27 +0530

    Update secondActivity.java

--- labsheet/labsheet04/hwex2/app/src/main/java/com/example/hwex2/secondActivity.java
+++ labsheet/labsheet04/hwex2/app/src/main/java/com/example/hwex2/secondActivity.java
@@ -3,12 +3,31 @@ package com.example.hwex2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class secondActivity extends AppCompatActivity {
+
+
+
+    private EditText et1,et2,et3,et4,et5;
+    private Button calculatebtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+        et1=(EditText) findViewById(R.id.txtitem1);
+        et2=(EditText) findViewById(R.id.txtitem2);
+        et3=(EditText) findViewById(R.id.txtitem3);
+        et4=(EditText) findViewById(R.id.txtitem4);
+        et5=(EditText) findViewById(R.id.txtitem5);
+
+        
+
+
     }
 }
