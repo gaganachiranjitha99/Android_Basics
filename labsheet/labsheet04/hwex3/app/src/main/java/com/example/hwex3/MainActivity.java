54f84d2e0680d7f9227ae40660c3ee138e3044a1
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-20 13:51:14 +0530

    Update MainActivity.java

--- labsheet/labsheet04/hwex3/app/src/main/java/com/example/hwex3/MainActivity.java
+++ labsheet/labsheet04/hwex3/app/src/main/java/com/example/hwex3/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.hwex3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button  gobtn;
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gobtn = (Button)findViewById(R.id.btngo);
+        display=
+
+
     }
 }
