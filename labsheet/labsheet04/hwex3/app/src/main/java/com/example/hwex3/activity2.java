bbd861f93bea703ddfcc85a70f03470554e0be8e
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-20 14:12:19 +0530

    Update activity2.java

--- labsheet/labsheet04/hwex3/app/src/main/java/com/example/hwex3/activity2.java
+++ labsheet/labsheet04/hwex3/app/src/main/java/com/example/hwex3/activity2.java
@@ -2,7 +2,9 @@ package com.example.hwex3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,12 +18,51 @@ public class activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        txtcolour=(EditText) findViewById(R.id.txtcolourcode);
-        searchbtn=(Button) findViewById(R.id.btnsearch);
+        txtcolour = (EditText) findViewById(R.id.txtcolourcode);
+        searchbtn = (Button) findViewById(R.id.btnsearch);
 
-        
+        searchbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String colourCode = txtcolour.getText().toString();
+                String cylinderInfo = getCylinderInfo(colourCode);
+
+                Intent intent =
+
+            }
+        });
 
 
+    }
 
+    private String getCylinderInfo(String colourCode) {
+
+        String fcylinderInfo;
+
+        switch (colourCode) {
+
+            case "red":
+                fcylinderInfo = "Red cylinder";
+                break;
+            case "green":
+                fcylinderInfo = "Green cylinder";
+                break;
+
+            case "blue":
+                fcylinderInfo = "Blue cylinder";
+                break;
+            case "pink":
+                fcylinderInfo = "Pink cylinder";
+                break;
+            case "white":
+                fcylinderInfo = "White cylinder";
+                break;
+            default:
+                fcylinderInfo = "colour code not found";
+                break;
+
+        }
+        return fcylinderInfo;
     }
 }
