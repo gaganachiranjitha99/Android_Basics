d72a4561fede450fca36b6ccf2d330f2e8421c5e
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-20 13:30:13 +0530

    activity 2

---
+++ labsheet/labsheet04/hwex3/app/src/main/java/com/example/hwex3/activity2.java
@@ -0,0 +1,14 @@
+package com.example.hwex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
