9931490d2d28b2b8d270e4f34ad86880966d1364
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-20 14:47:34 +0530

    Update MainActivity.java

--- labsheet/labsheet04/hwex4/app/src/main/java/com/example/hwex4/MainActivity.java
+++ labsheet/labsheet04/hwex4/app/src/main/java/com/example/hwex4/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.hwex4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView email;
+    TextView phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email=(TextView) findViewById(R.id.txtemail);
+
+
+
     }
 }
