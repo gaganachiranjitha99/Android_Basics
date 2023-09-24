16d95c35f898a751dd749da1ad14ea15097db2f0
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-24 23:16:35 +0530

    updated

--- labsheet/labsheet05/ex2/app/src/main/java/com/example/ex2/MainActivity.java
+++ labsheet/labsheet05/ex2/app/src/main/java/com/example/ex2/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//wdefw
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
