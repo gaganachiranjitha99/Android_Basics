25e77e8dcb4459d2485998b638ca22ea5fb3fc6d
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-05 14:34:06 +0530

    Update MainActivity.java

--- labsheet/labsheet06/ex2/app/src/main/java/com/example/ex2/MainActivity.java
+++ labsheet/labsheet06/ex2/app/src/main/java/com/example/ex2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ex2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -30,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
 
         }
         FragmentManager fm =getSupportFragmentManager();
-        
+        FragmentTransaction fragmentTransaction= fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_section,fr);
+
+        fragmentTransaction.commit();
 
 
 
