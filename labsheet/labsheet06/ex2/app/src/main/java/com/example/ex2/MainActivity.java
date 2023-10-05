3e9fa8bdf886bc6ca4d760c90717356d40224ce8
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-05 14:29:32 +0530

    main java

--- labsheet/labsheet06/ex2/app/src/main/java/com/example/ex2/MainActivity.java
+++ labsheet/labsheet06/ex2/app/src/main/java/com/example/ex2/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void selectFragment(View view){
+        //creating the object for fragment
+        Fragment fr;
+
+        //displaying first fragment
+        //if button 1 is clicked
+        if(view == findViewById(R.id.button1)){
+
+            fr=new FragmentOne();
+        }else{
+
+            fr=new FragmentTwo();
+
+        }
+        FragmentManager fm =getSupportFragmentManager();
+        
+
+
+
+
+
+    }
+
+
 }
