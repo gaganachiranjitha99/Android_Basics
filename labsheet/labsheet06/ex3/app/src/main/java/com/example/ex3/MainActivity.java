d198f6bb4c779e0128b5f080fc460d3d71a7a82e
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-05 15:26:38 +0530

    list view completed

--- labsheet/labsheet06/ex3/app/src/main/java/com/example/ex3/MainActivity.java
+++ labsheet/labsheet06/ex3/app/src/main/java/com/example/ex3/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.ex3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView MonthList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] List={"jan","feb","march","april","may","june","july","aug","sep","oct","nov","dec",
+                "jan","feb","march","april","may","june","july","aug","sep","oct","nov","dec"};
+
+        ArrayAdapter Months=new ArrayAdapter<String>(this,R.layout.list_row,R.id.months,List);
+        MonthList=(ListView) findViewById(R.id.listMonths);
+        MonthList.setAdapter(Months);
     }
 }
