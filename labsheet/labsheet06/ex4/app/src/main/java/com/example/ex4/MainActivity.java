45e609c06b0b2645b9550487113e932e990e337c
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-05 21:03:45 +0530

    updated

--- labsheet/labsheet06/ex4/app/src/main/java/com/example/ex4/MainActivity.java
+++ labsheet/labsheet06/ex4/app/src/main/java/com/example/ex4/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.ex4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
+    TextView textView;
+    String[] carBrands={
+
+            "Ferrari",
+            "Mclaren",
+            "Jaguar",
+            "Skoda",
+            "Susuki",
+            "Hyhndai",
+            "Mercedes",
+            "Bmw",
+            "Ford",
+            "Honda",
+            "Chevrolet",
+            "Volkwagon",
+
+
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gridView=(GridView) findViewById(R.id.grid);
+        textView=(TextView) findViewById(R.id.textview);
+
+        ArrayAdapter adapter=new ArrayAdapter<String>(this,R.id.textview,carBrands);
+        gridView.setAdapter(adapter);
+
     }
 }
