b0e1ed682290615e968aa54bf0116d70860cbc99
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-12 14:40:34 +0530

    Update MainActivity.java

--- labsheet/labsheet07/ex3/app/src/main/java/com/example/ex3/MainActivity.java
+++ labsheet/labsheet07/ex3/app/src/main/java/com/example/ex3/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.ex3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ListView simpleListview;
+    String[] animalName={"Rabbit","Squirrel","Dog","Tiger","Elephant","Deer"};
+
+
+    int[] animalName={R.drawable.animal1,R.drawable.animal2,
+    R.drawable.animal3,R.drawable.animal4,
+            R.drawable.animal5,R.drawable.animal6
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        simpleListview =(ListView) findViewById(R.id.simpleListView);
+        ArrayList<HashMap<String,String>> arrayList=new HashMap<>();
+        for (int i=0;i<animalName.length;i++){
+            HashMap<String,String> hashMap =new HashMap<>();
+            hashMap.put("name",animalName[i]);
+        }
     }
 }
