3c4ea1a7dc53e1b64650481889f651266dc25f09
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-12 14:25:15 +0530

    listview items

---
+++ labsheet/labsheet07/ex3/app/src/main/java/com/example/ex3/list_view_items.java
@@ -0,0 +1,14 @@
+package com.example.ex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_view_items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_items);
+    }
+}
