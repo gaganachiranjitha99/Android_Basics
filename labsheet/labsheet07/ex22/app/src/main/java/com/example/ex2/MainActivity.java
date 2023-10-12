0869c6c39a5ec6996f28675736262f345bb4a71d
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-12 14:14:50 +0530

    array adaptor completed

--- labsheet/labsheet07/ex22/app/src/main/java/com/example/ex2/MainActivity.java
+++ labsheet/labsheet07/ex22/app/src/main/java/com/example/ex2/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements Adapter, AdapterView.OnItemSelectedListener {
 
@@ -29,11 +32,61 @@ public class MainActivity extends AppCompatActivity implements Adapter, AdapterV
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        
+        Toast.makeText(getApplicationContext(),animalsList[position],Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
     }
 }
