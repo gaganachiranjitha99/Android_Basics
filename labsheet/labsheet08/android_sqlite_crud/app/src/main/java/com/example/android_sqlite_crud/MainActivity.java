3d46edc5d20f6aa9b807a4ee24c08910c1185f1e
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-21 23:55:52 +0530

    data insert

--- labsheet/labsheet08/android_sqlite_crud/app/src/main/java/com/example/android_sqlite_crud/MainActivity.java
+++ labsheet/labsheet08/android_sqlite_crud/app/src/main/java/com/example/android_sqlite_crud/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.android_sqlite_crud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button insert,update,delete,view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        insert=(Button) findViewById(R.id.insertbtn);
+        update=(Button) findViewById(R.id.updatebtn);
+        delete=(Button) findViewById(R.id.deletebtn);
+        view=(Button) findViewById(R.id.viewbtn);
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), activity_add.class));
+            }
+        });
     }
 }
--- labsheet/labsheet08/android_sqlite_crud/app/src/main/java/com/example/android_sqlite_crud/activity_add.java
+++ labsheet/labsheet08/android_sqlite_crud/app/src/main/java/com/example/android_sqlite_crud/activity_add.java
@@ -3,12 +3,43 @@ package com.example.android_sqlite_crud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class activity_add extends AppCompatActivity {
+
+    EditText name;
+    EditText dep;
+    EditText sal;
+    Button btnadd;
+    DBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        name=(EditText) findViewById(R.id.txtname);
+        dep=(EditText) findViewById(R.id.txtdep);
+        sal=(EditText) findViewById(R.id.txtsalary);
+        btnadd=(Button) findViewById(R.id.addbtn);
+        dbHandler=new DBHandler(this);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String empname=name.getText().toString();
+                String empdep=dep.getText().toString();
+                String empsal=sal.getText().toString();
+                Employee emp= new Employee(empname,empdep,empsal);
+                dbHandler.addEmployee(emp);
+                Toast.makeText(getApplicationContext(),"Data inserted",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
     }
 }
