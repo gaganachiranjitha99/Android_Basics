510afbcaeb17be4815d25bb81de98346a40e6a0e
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-21 22:50:13 +0530

    activity add

---
+++ labsheet/labsheet08/android_sqlite_crud/app/src/main/java/com/example/android_sqlite_crud/activity_add.java
@@ -0,0 +1,14 @@
+package com.example.android_sqlite_crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
